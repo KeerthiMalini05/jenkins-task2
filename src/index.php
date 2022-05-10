4c28f00ddbddf10f9ea2ead3250ab52ec58c179c
KeerthiMalini05 <58335834+KeerthiMalini05@users.noreply.github.com>
2022-05-10 12:44:25 +0530

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<?php
+$host=gethostbyname("db");
+echo '<h4> Docker-assignment2->Sucessfully connected to mysql"</h4>';
+
+$conn = new mysqli('db','keerthi','Keerthi05');
+if ($conn->connect_error) {
+    die('Could not connect: ' . $conn->connect_error);
+}
+echo 'Connected successfully';
+?>
